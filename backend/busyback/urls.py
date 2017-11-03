8f8f05542992e3243fe409066720afebb7dc6201
yuyupopo <yuyupopo@snu.ac.kr>
2017-11-03 22:33:52 +0900

    initialized repository

---
+++ backend/busyback/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from blog import views
+
+urlpatterns = [
+    url('^signup$', views.signup, name='signup'),
+    url('^signin$', views.signin, name='signin'),
+    url('^signout$', views.signout, name='signout'),
+    url('^token$', views.token, name='token'),
+]
